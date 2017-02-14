9203262015817b51902bd56a4c16e7884224b259
Felipe Cancino <felipe.cancinos@gmail.com>
2017-02-13 23:19:37 -0700

    NeuralPart2

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,15 +6,17 @@ from sklearn import preprocessing
 import numpy as np
 import pandas as pd
 import random
+import math
+
 
 class Neuron:
     def __init__(self, numOfInputs):
-        self.weights = 2 * np.random.random_sample(numOfInputs) - 1
+        self.weights = 2 * np.random.random_sample(numOfInputs + 1) - 1
         self.prediction = None
 class NeuralNetwork:
 
-    def __init__(self):
-
+    def __init__(self, number_of_layers, nodes_in_layer):
+        self.array_of_predictions = []
         temp_data = []
         temp = []
         with open('iris.csv', newline='') as csvfile:
@@ -42,41 +44,94 @@ class NeuralNetwork:
         normalized_data_instances = preprocessing.normalize(data_instances)
 
         self.data_train, self.data_test, self.target_train, self.target_test = tts(normalized_data_instances, targets, train_size=.7)
-        array_of_1 = np.empty(len(self.data_train))
-        array_of_1.fill(-1)
-        self.data_train = np.hstack((self.data_train, np.atleast_2d(array_of_1).T))
-        print(self.data_train)
+        #array_of_1 = np.empty(len(self.data_train))
+        #array_of_1.fill(-1)
+        #self.data_train = np.hstack((self.data_train, np.atleast_2d(array_of_1).T))
+        #print(self.data_train)
         dict_counter_target = Counter(self.target_train)
         target_counter = 0
         for i in dict_counter_target.keys():
             target_counter += 1
 
         self.num_of_targets = target_counter
-        self.neuron_layers = [Neuron(len(self.data_train[0])) for i in range(self.num_of_targets)]
+        self.layers = []
+        self.nodes_in_layer = nodes_in_layer
 
+        for i in range(1, number_of_layers):
+            if i == 1:
+                self.layers.append([Neuron(len(self.data_train[0])) for i in range(self.nodes_in_layer)])
+            else:
+                self.layers.append([Neuron(self.nodes_in_layer) for i in range(self.nodes_in_layer)])
+
+        self.layers.append([Neuron(self.nodes_in_layer) for i in range(self.num_of_targets)])
+
+
+
+    def sigmoid(self, x):
+        return 1 / (1 + math.exp(-x))
 
     def train(self):
-        for i in range(len(self.neuron_layers)):
-            value = np.dot(self.data_train[0], self.neuron_layers[i].weights)
-            self.neuron_layers[i].prediction = value >= 0
+        # for i in range(len(self.neuron_layer1)):
+        #     value = np.dot(self.data_train[0], self.neuron_layer1[i].weights)
+        #     self.neuron_layer1[i].prediction = self.calculate_sigma(value)
+
+        #train the first layer
+            for n in self.data_train:
+                for i in self.layers[0]:
+                    value = np.dot(np.append(n, - 1), i.weights)
+                    i.prediction = self.sigmoid(value)
+
+
+                for i in range(1, len(self.layers)):
+                    predictions = []
+                    for n in self.layers[i-1]:
+                        predictions.append(n.prediction)
+
+                    predictions = np.array(predictions)
+
+
+                    for n in self.layers[i]:
+                        value = np.dot(np.append(predictions, -1), n.weights)
+                        n.prediction = self.sigmoid(value)
+                        print(n.prediction)
+
+                predicted_values = []
+                for i in self.layers[len(self.layers) - 1]:
+                    predicted_values.append(i.prediction)
+
+                predicted_values = np.array(predicted_values)
+                prediction = predicted_values.argmax()
+
+                self.array_of_predictions.append(prediction)
+
+
+
+        # list_of_predictions = []
+        # for i in range(len(self.neuron_layer1)):
+        #     list_of_predictions.append(i)
+        #
+        # print(list_of_predictions)
+
 
     def predict(self):
-        prediction_num = 0
-        for i in range(len(self.neuron_layers)):
-            if self.neuron_layers[i].prediction:
-                prediction_num += 1
+      pass
+    def calc_percentage(self):
 
-        if prediction_num == 0:
-            return "Type 0"
-        elif prediction_num == 1:
-            return "Type 1"
-        else:
-            return "Other type"
-
+        sum = 0
+        print(self.array_of_predictions)
+        for i in range(len(self.target_train)):
+            if self.target_train[i] == str(self.array_of_predictions[i]):
+                sum += 1
+        print(self.target_train)
+        return (sum * 100)/ len(self.array_of_predictions)
 
 
 
-neuralNetwork = NeuralNetwork()
+# for i, value in enumerate(["milk", "bread", "cheese"]):
+#   exec ("var%s=value" % (i))
 
+
+neuralNetwork = NeuralNetwork(3, 4)
 neuralNetwork.train()
-print(neuralNetwork.predict())
+print(neuralNetwork.calc_percentage())
+
